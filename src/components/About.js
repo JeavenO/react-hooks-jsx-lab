68e57405c93cc71c242816e6ffd8f30ea493b42a
Jeaven <jeaveno123@gmail.com>
2025-10-26 11:40:44 +0300

    Completed JSX structure for Navbar, Home, and About components

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { image } from "../data/data";
+// Note: The prompt showed 'image' from '../data/data'
+import { image } from "../data/data"; 
 
 function About() {
-  return <div>About</div>;
+  // update the JSX being returned!
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I am building a portfolio website with React and JSX!</p> 
+      {/* The src must be the imported image variable */}
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
-import { name, city } from "../data/data.js";
+// Note: The prompt showed 'name' and 'city' from '../data/data.js'
+import { name, city } from "../data/data.js"; 
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      <h1 style={{ color: "firebrick" }}>
+        {/* Use JSX expression syntax {} for variables */}
+        {name} is a Web Developer from {city}
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
